128e5f3c04d281d5277bc9a179df20ed1ca168d7
Mahmoud Sadrian <83926434+mahmoud-sdr@users.noreply.github.com>
2025-08-05 03:02:51 +0330

    Update NamePrefixRemover.py
    return lower format of name in clean_name()

--- NamePrefixRemover.py
+++ NamePrefixRemover.py
@@ -19,7 +19,7 @@ def clean_name(name: str) -> str:
     while old != name:
         old = name
         name = re.sub(clean_name_pattern, '', name, flags=re.IGNORECASE).strip()
-    return name
+    return name.lower()
 
 # test_names = [
 #     "Dr. Jane Doe",
