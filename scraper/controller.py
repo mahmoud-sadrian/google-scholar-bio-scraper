a82767f94ecb73518a58e25ffa45b6ee9fc3c01a
Mahmoud Sadrian <83926434+mahmoud-sdr@users.noreply.github.com>
2025-08-10 00:15:54 +0330

    Update controller.py
    - add enter ID and URL in EnterCommand(Command)

--- scraper/controller.py
+++ scraper/controller.py
@@ -13,18 +13,31 @@ class Command(ABC):
 class EnterCommand(Command):
     def execute(self):
         print("Enter new search criteria (leave blank to skip) ↓\n")
+
+        id = query_state.input_ID()
+        if id:
+            query_state.update("ID", id)
+
+        url = query_state.input_URL()
+        if url:
+            query_state.update("URL", url)
+
         name = query_state.input_name()
         if name:
             query_state.update("name", name)
+
         email = query_state.input_email()
         if email:
             query_state.update("email", email)
+
         affiliation = query_state.input_affiliation()
         if affiliation:
             query_state.update("affiliation", affiliation)
+
         interests = query_state.input_interests()
         if interests:
             query_state.update("interests", interests)
+
         print("\n✅ Search criteria updated.")
 
 class ViewCommand(Command):
