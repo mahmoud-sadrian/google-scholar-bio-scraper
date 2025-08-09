a6a2e46bc75a28334140676dc5ee5736a037d454
Mahmoud Sadrian <83926434+mahmoud-sdr@users.noreply.github.com>
2025-08-09 18:36:18 +0330

    Update view.py
    Epic change (state). It is not complete yet.
    
    bacause view method in QueryState class just return sth I should add some prints in this file.
    
    Will complete soon...

--- scraper/criteria/view.py
+++ scraper/criteria/view.py
@@ -1,8 +1,2 @@
-def handle(query: dict):
-    if not query:
-        print("ℹ️  No search criteria set.")
-        return
-
-    print("📄 Current search criteria:\n")
-    for key, value in query.items():
-        print(f"• {key.capitalize()}: {value}")
+def handle(query_state):
+    print(query_state.view())
